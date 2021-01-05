1352633c745da9dca2ca186b5f119fc8acc458d8
lindx <linder.igor@gmail.com>
2021-01-05 13:13:05 +0300

    adress.add and address.getall

--- src/main/java/com/lindx/example/Main.java
+++ src/main/java/com/lindx/example/Main.java
@@ -3,11 +3,12 @@ package com.lindx.example;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import com.lindx.example.util.Util;
+import com.lindx.example.entity.Address;
+import com.lindx.example.service.AddressService;
 
-import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.List;
 
 public class Main {
 
@@ -15,7 +16,18 @@ public class Main {
     static Statement statement = null;
     public static void main(String[] args) {
         
-        connection = Util.getConnect();
+        Address address = new Address();
+            address.setId(1L);
+            address.setCountry("USA");
+            address.setCity("California");
+            address.setStreet("Stars");
+            address.setPostCode("123-456-789");
+        
+        AddressService addressService = new AddressService();
+
+       List<Address> addresses =  addressService.getAll();
+
+       System.out.println(addresses);
        
     }
 }
--- src/main/java/com/lindx/example/util/Util.java
+++ src/main/java/com/lindx/example/util/Util.java
@@ -3,25 +3,25 @@ package com.lindx.example.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class Util {
 
     private static Connection connection = null;
-    
-    public static Connection getConnect() {
-        
-        String url = "jdbc:mysql://localhost:3306/testbase";
+
+    public static Connection getConnection() {
+
+        String url = "jdbc:mysql://localhost:3306/test";
         String user = "lindx";
         String pass = "MySql12345!";
         String param = "?autoReconnect=true&useSSL=false";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(url+param, user, pass);
+            connection = DriverManager.getConnection(url + param, user, pass);
             System.out.println("Connection Succsesfull");
+            
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } 
+        }
         return connection;
     }
 }
