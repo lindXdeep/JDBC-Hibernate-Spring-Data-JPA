06623d9fbf0d4db0da37455e4db8f7c055881a63
lindx <linder.igor@gmail.com>
2021-01-12 22:26:35 +0300

    delete resources and add configuration java Properties

--- src/main/java/com/lindx/example/util/HibernateUtil.java
+++ src/main/java/com/lindx/example/util/HibernateUtil.java
@@ -1,19 +1,47 @@
 package com.lindx.example.util;
 
 import java.io.File;
+import java.util.Properties;
+
+import com.lindx.example.entity.Address;
+import com.lindx.example.entity.Employee;
+import com.lindx.example.entity.Project;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 public class HibernateUtil {
 
-    private static File cfg = new File("src/main/resources/hibernate.cfg.xml");
+    private static Properties properties = null;
+    private static Configuration cfg = null;
+
+    static {
+        properties = new Properties();
+        properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/test1?autoReconnect=true&amp;useSSL=false");
+        properties.setProperty(Environment.USER, "lindx");
+        properties.setProperty(Environment.PASS, "MySql12345!");
+        properties.setProperty(Environment.POOL_SIZE, "1");
+        properties.setProperty(Environment.AUTOCOMMIT, "false");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.setProperty(Environment.CACHE_PROVIDER_CONFIG, "org.hibernate.cache.NoCacheProvider");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+
+        cfg = new Configuration();
+        cfg.setProperties(properties);
+        cfg.addAnnotatedClass(Address.class);
+        cfg.addAnnotatedClass(Employee.class);
+        cfg.addAnnotatedClass(Project.class);
+    }
 
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
 
         try {
-            return new Configuration().configure(cfg).buildSessionFactory();
+            return cfg.buildSessionFactory();
         }catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
